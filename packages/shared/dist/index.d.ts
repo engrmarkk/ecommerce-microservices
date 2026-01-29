ee273288a96367c0f5ea6c97f1a36c228723483d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-29 15:10:40 +0100

    Update index.d.ts

--- packages/shared/dist/index.d.ts
+++ packages/shared/dist/index.d.ts
@@ -1,1 +1,5 @@
 export * from './types/misc.types';
+export * from './types/auth.types';
+export * from './utils/statusResponse';
+export * from './utils/statusCodes';
+export * from './utils/apiResponse';
