eebd1a669e78c44bd3a905ae670abd4e0dca9741
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-29 15:11:26 +0100

    Create statusResponse.js

---
+++ packages/shared/dist/utils/statusResponse.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HttpStatusMessages = void 0;
+class HttpStatusMessages {
+}
+exports.HttpStatusMessages = HttpStatusMessages;
+HttpStatusMessages.SUCCESS = 'success';
+HttpStatusMessages.FAILED = 'failed';
+HttpStatusMessages.PENDING = 'pending';
+HttpStatusMessages.ERROR = 'error';
