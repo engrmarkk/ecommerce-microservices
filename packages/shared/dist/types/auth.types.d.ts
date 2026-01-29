1e83cfc607cb94328608bc04fd021e328351c742
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-29 15:10:54 +0100

    Create auth.types.d.ts

---
+++ packages/shared/dist/types/auth.types.d.ts
@@ -0,0 +1,11 @@
+export interface ILoginRequest {
+    email: string;
+    password: string;
+}
+export interface IRegisterRequest {
+    email: string;
+    password: string;
+    confirmPassword: string;
+    firstName: string;
+    lastName: string;
+}
