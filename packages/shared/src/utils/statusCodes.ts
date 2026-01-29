f09164a076f815d42ac4eeb50f87fc4b0b8f3796
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-29 15:11:51 +0100

    Create statusCodes.ts

---
+++ packages/shared/src/utils/statusCodes.ts
@@ -0,0 +1,27 @@
+export class HttpStatusCodes {
+  static CONTINUE = 100;
+  static OK = 200;
+  static CREATED = 201;
+  static ACCEPTED = 202;
+  static NON_AUTHORITATIVE_INFORMATION = 203;
+  static NO_CONTENT = 204;
+  static MULTIPLE_CHOICES = 300;
+  static MOVED_PERMANENTLY = 301;
+  static FOUND = 302;
+  static BAD_REQUEST = 400;
+  static UNAUTHORIZED = 401;
+  static PAYMENT_REQUIRED = 402;
+  static FORBIDDEN = 403;
+  static NOT_FOUND = 404;
+  static METHOD_NOT_ALLOWED = 405;
+  static REQUEST_TIMEOUT = 408;
+  static CONFLICT = 409;
+  static UNPROCESSABLE_ENTITY = 422;
+  static TOO_MANY_REQUESTS = 429;
+  static REQUEST_HEADER_FIELDS_TOO_LARGE = 431;
+  static INTERNAL_SERVER_ERROR = 500;
+  static NOT_IMPLEMENTED = 501;
+  static BAD_GATEWAY = 502;
+  static SERVICE_UNAVAILABLE = 503;
+  static GATEWAY_TIMEOUT = 504;
+}
