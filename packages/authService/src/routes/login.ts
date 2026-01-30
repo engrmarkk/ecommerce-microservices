17bd84547aa4791cbcec210887486382f917f18f
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-30 12:17:56 +0100

    changed auth to user

--- packages/authService/src/routes/login.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import loginController from '../controllers/authControllers/loginControl';
-
-const router: Router = Router();
-
-// login route, POST
-router.post('/', loginController);
-
-export default router;
--- packages/authService/src/routes/register.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import registerController from '../controllers/authControllers/registerControl';
-
-const router: Router = Router();
-
-// login route, POST
-router.post('/', registerController);
-
-export default router;
