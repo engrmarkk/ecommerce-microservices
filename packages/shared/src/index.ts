f7b949ff995d9d1d09b9cf44a323497e356483e0
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-01-30 12:18:35 +0100

    Update index.ts

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,3 +3,5 @@ export * from './types/auth.types';
 export * from './utils/statusResponse';
 export * from './utils/statusCodes';
 export * from './utils/apiResponse';
+export * from './middlewares/authHandler';
+export * from './types/user.types';
